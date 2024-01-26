e615070e10bd757fb62cd6e1e7afac6fbbe7b859
Adil Soomro <adilgulsoomro@gmail.com>
2024-01-26 13:42:46 -0500

    Fourth Commit

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ $(document).ready(function () {
     // Make a GET request to the JSON API using jQuery's .getJSON method
     $.getJSON("schedule.json", function (data) {
         // Store the schedule data in a global variable
-        let scheduleData = data.schedule;
+        let myScheduleData = data.mySchedule;
+        let ethanScheduleData = data.ethanSchedule;
+        let samScheduleData = data.samSchedule;
         // Populate the table with all the schedule data
-        populateTable(scheduleData);
+        console.log(myScheduleData, ethanScheduleData, samScheduleData);
+        populateTable(myScheduleData, ethanScheduleData, samScheduleData);
 
         // Listen for changes to the dropdown menu
         $('#day-selector').on('change', function() {
@@ -13,18 +16,47 @@ $(document).ready(function () {
             let selectedDay = $(this).val();
 
             // Filter the schedule data based on the selected day
-            let filteredData = scheduleData.filter( function (schedule) {
-                return selectedDay === 'all' || schedule.days.includes(selectedDay);
+            let myFilteredData = myScheduleData.filter( function (mySchedule) {
+                return selectedDay === 'all' || mySchedule.days.includes(selectedDay);
+            })
+            let ethanFilteredData = ethanScheduleData.filter(function (ethanSchedule) {
+                return selectedDay === 'all' || ethanSchedule.days.includes(selectedDay);
+            })
+            let samFilteredData = samScheduleData.filter(function (samSchedule) {
+                return selectedDay === 'all' || samSchedule.days.includes(selectedDay);
             })
         })
     });
-
     // Function to populate the table with schedule data
-    function populateTable(scheduleData) {
+    function populateTable(myScheduleData, ethanScheduleData, samScheduleData) {
         $('#schedule-table.body').empty();
 
-        // Iterate over each object om tje schedule Data array
-        $.each(scheduleData, function (i, schedule) {
+        // Iterate over each object on the schedule Data array
+        $.each(myScheduleData, function (i, schedule) {
+            // Create a new row in the table
+            let row = "<tr>";
+            // Add the class name, teacher name, room number, and days to the row
+            row += "<td>" + schedule.class_name + '</td>';
+            row += '<td>' + schedule.teacher_name + '</td>'
+            row += '<td>' + schedule.room_number + '</td>'
+            row += '<td>' + schedule.days.join(', ') + '</td>'
+            row += '</tr>'
+
+            $('#schedule-table-body').append(row)
+        })
+        $.each(ethanScheduleData, function (i, schedule) {
+            // Create a new row in the table
+            let row = "<tr>";
+            // Add the class name, teacher name, room number, and days to the row
+            row += "<td>" + schedule.class_name + '</td>';
+            row += '<td>' + schedule.teacher_name + '</td>'
+            row += '<td>' + schedule.room_number + '</td>'
+            row += '<td>' + schedule.days.join(', ') + '</td>'
+            row += '</tr>'
+
+            $('#schedule-table-body').append(row)
+        })
+        $.each(samScheduleData, function (i, schedule) {
             // Create a new row in the table
             let row = "<tr>";
             // Add the class name, teacher name, room number, and days to the row
